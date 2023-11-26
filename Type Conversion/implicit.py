d79beba26d947704fe92555bb18054627e9efe95
Nisha Kuvalekar <121301819+nishakuvalekar25@users.noreply.github.com>
2023-11-26 16:49:30 +0530

    Add files via upload

---
+++ Type Conversion/implicit.py
@@ -0,0 +1,11 @@
+# Implicit Type Conversion
+
+integer_num = 123
+float_num = 1.23
+
+new_num = integer_num + float_num
+
+# Display new value and resulting data type
+
+print("Value:",new_num)
+print("Data Type:",type(new_num))
