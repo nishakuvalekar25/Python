988fe11ac20d917fc64e2aff07d48089c9dfc01f
Nisha Kuvalekar <121301819+nishakuvalekar25@users.noreply.github.com>
2023-12-02 22:35:21 +0530

    Add files via upload

---
+++ Control Flow/pass.py
@@ -0,0 +1,16 @@
+num = 5
+if num > 10:
+    pass
+else:
+    print("num is less than or equal to 10")
+ 
+# Using pass in a function definition as a placeholder for implementation
+
+def myfunction():
+    pass
+ 
+# Using pass in a function definition as a placeholder for implementation
+ 
+class MyClass:
+    def __init__(self):
+        pass
