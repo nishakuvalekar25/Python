cfd73767c5e5028d5da8f70287e8d75fa2dfe082
Nisha Kuvalekar <121301819+nishakuvalekar25@users.noreply.github.com>
2023-12-03 10:56:30 +0530

    Update try_except.py

--- Exception Handling/try_except.py
+++ Exception Handling/try_except.py
@@ -1,10 +1,13 @@
 a = 10
 b = 0
 
+# The try block contains the code that might generate an exception
 try:
     result = a/b
     print(result)
-
+    
+# When an exception occurs, it is caught by the except block. 
+# The except block cannot be used without the try block.
 except:
     print("Error: Denominator cannot be 0.")
 
