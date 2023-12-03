15ada0207617ca2acecc8eed6866a0449c48dde1
Nisha Kuvalekar <121301819+nishakuvalekar25@users.noreply.github.com>
2023-12-03 11:06:35 +0530

    Update try_with_else.py

--- Exception Handling/try_with_else.py
+++ Exception Handling/try_with_else.py
@@ -1,6 +1,8 @@
+# In some situations, we might want to run a certain block of code if the code block inside try runs without any errors.
+# For these cases, you can use the optional else keyword with the try statement. 
+
 def divide(x, y): 
     try: 
-       
         result = x // y 
         
     except ZeroDivisionError: 
@@ -10,3 +12,7 @@ def divide(x, y):
    
 divide(3, 2) 
 divide(3, 0)
+
+# Output
+# Yeah ! Your answer is : 1
+# Sorry ! You are dividing by zero
