2de49b9ab53558eadc6ce0df79577597de3f86f5
Nisha Kuvalekar <121301819+nishakuvalekar25@users.noreply.github.com>
2023-12-25 08:23:01 +0530

    Add files via upload

---
+++ Python Scope/Global_scope.py
@@ -0,0 +1,18 @@
+# Global Scope
+
+# A variable created in the main body of the Python code is a global variable and belongs to the global scope.
+#Global variables are available from within any scope, global and local.
+
+x = 300
+
+def myfunc():
+  print(x)
+
+myfunc()
+
+print(x)
+
+# Output : 
+
+# 300
+# 300
